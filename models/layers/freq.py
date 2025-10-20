fd500643a0b69851a4a27ecc7c47916332255693
Kohn24 <2519762723@qq.com>
2025-10-20 19:56:30 +0800

    new start get rid of recbole_cdr  TODO: yaml config、new infra

---
+++ models/layers/freq.py
@@ -0,0 +1,51 @@
+
+import math
+import torch
+import torch.nn as nn
+import torch.fft as fft
+
+
+class BandedFourierLayer(nn.Module):
+    """
+
+    """
+    def __init__(self, in_channels, out_channels, band, num_bands, length=201):
+        super().__init__()
+
+        self.length = length
+        self.total_freqs = (self.length // 2) + 1
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        self.band = band  # zero indexed
+        self.num_bands = num_bands
+
+        self.num_freqs = self.total_freqs // self.num_bands + (
+            self.total_freqs % self.num_bands if self.band == self.num_bands - 1 else 0)
+
+        self.start = self.band * (self.total_freqs // self.num_bands)
+        self.end = self.start + self.num_freqs
+
+        # case: from other frequencies
+        self.weight = nn.Parameter(torch.empty((self.num_freqs, in_channels, out_channels), dtype=torch.cfloat))
+        self.bias = nn.Parameter(torch.empty((self.num_freqs, out_channels), dtype=torch.cfloat))
+        self.reset_parameters()
+
+    def forward(self, input):
+        # input - b t d
+        b, t, _ = input.shape
+        input_fft = fft.rfft(input, dim=1)
+        output_fft = torch.zeros(b, t // 2 + 1, self.out_channels, device=input.device, dtype=torch.cfloat)
+        output_fft[:, self.start:self.end] = self._forward(input_fft)
+        return fft.irfft(output_fft, n=input.size(1), dim=1)
+
+    def _forward(self, input):
+        output = torch.einsum('bti,tio->bto', input[:, self.start:self.end], self.weight)
+        return output + self.bias
+
+    def reset_parameters(self) -> None:
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+        fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
+        bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+        nn.init.uniform_(self.bias, -bound, bound)
