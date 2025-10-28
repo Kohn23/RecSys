c26ef0d5c80da2de9b161d7fce463abf27d4d7c7
Kohn24 <2519762723@qq.com>
2025-10-28 12:27:30 +0800

    views augment

--- utils/dataloader/mixins/data_augment_mixin.py
+++ utils/dataloader/mixins/data_augment_mixin.py
@@ -4,6 +4,7 @@ Data Augment Mixins only provide augment frameworks, specific methods should be 
 
 import random
 import torch
+from typing import Dict, Any, Optional
 from recbole.data.interaction import Interaction
 from abc import ABC, abstractmethod
 from utils.dataloader.functional import (
@@ -12,6 +13,7 @@ from utils.dataloader.functional import (
     seq_item_noise,
     seq_item_reorder
 )
+
 
 class DataAugmentMixin(ABC):
     """Interface"""
@@ -46,7 +48,7 @@ class SequentialDataAugmentMixin(DataAugmentMixin):
 
     def __init__(self, config):
         super().__init__(config)
-        self.augment_method = self._get_method(config)
+        self.augment_method: Optional[Dict[str, Any]] = self._get_method(config)
 
     def _set_params(self, config, augment_method):
         self.ITEM_SEQ = config["ITEM_ID_FIELD"] + config["LIST_SUFFIX"]
@@ -94,12 +96,80 @@ class SequentialDataAugmentMixin(DataAugmentMixin):
         return augment_method
 
     def _get_single_view(self, seq, length):
+        if length <= 1:
+            return seq.clone(), length.clone()
 
-    def _get_views(self, seq, length):
+        available_funcs = self.augment_method['func']
+        chosen_func = random.choice(available_funcs)
+        augment_func = self.map_function[chosen_func]
 
+        if chosen_func == 'crop':
+            aug_seq, aug_len = augment_func(seq, length, self.crop_eta)
+        elif chosen_func == 'mask':
+            aug_seq, aug_len = augment_func(seq, length, self.mask_gamma)
+        elif chosen_func == 'reorder':
+            aug_seq, aug_len = augment_func(seq, length, self.reorder_beta)
+        else:
+            aug_seq, aug_len = augment_func(seq, length)
+
+        return aug_seq, aug_len
+
+    def _get_views(self, interaction):
+        """views for contrastive learning"""
+        seqs = interaction[self.ITEM_SEQ]
+        lengths = interaction[self.ITEM_SEQ_LEN]
+        batch_size = len(seqs)
+
+        if self.mode == 'single-view':
+            aug_seqs = []
+            aug_lens = []
+
+            for i in range(batch_size):
+                seq = seqs[i]
+                length = lengths[i]
+                aug_seq, aug_len = self._get_single_view(seq, length)
+                aug_seqs.append(aug_seq)
+                aug_lens.append(aug_len)
+
+            # 更新interaction
+            interaction.update(Interaction({
+                'aug_view': torch.stack(aug_seqs),
+                'aug_len': torch.stack(aug_lens)
+            }))
+        elif self.mode == 'dual-view':
+            aug_seqs1 = []
+            aug_lens1 = []
+            aug_seqs2 = []
+            aug_lens2 = []
+
+            for i in range(batch_size):
+                seq = seqs[i]
+                length = lengths[i]
+                aug_seq1, aug_len1 = self._get_single_view(seq, length)
+                aug_seq2, aug_len2 = self._get_single_view(seq, length)
+
+                aug_seqs1.append(aug_seq1)
+                aug_lens1.append(aug_len1)
+                aug_seqs2.append(aug_seq2)
+                aug_lens2.append(aug_len2)
+
+            interaction.update(Interaction({
+                'aug1': torch.stack(aug_seqs1),
+                'aug_len1': torch.stack(aug_lens1),
+                'aug2': torch.stack(aug_seqs2),
+                'aug_len2': torch.stack(aug_lens2)
+            }))
+        else:
+            raise ValueError(f"Unsupported mode: {self.mode}")
+
+        return interaction
 
     def augment(self, interaction):
-        if 
+        if not self.augment_method:
+            return interaction
 
+        # contrastive learning views
+        if self.augment_method['mode'] == 'single-view' or self.augment_method['mode'] == 'dual-view':
+            interaction = self._get_views(interaction)
 
         return interaction
